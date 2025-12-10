098b027101c84d0e10b53db5e3b1e8e22cd07977
Fedorova_Darya <fdu12345@mail.ru>
2025-12-10 22:47:42 +0300

    --

--- Примеры/primer_6.py
+++ Примеры/primer_6.py
@@ -5,18 +5,8 @@ import sys
 
 if __name__ == '__main__':
     A = list(map(int, input().split()))
-
     if len(A) != 10:
         print("Неверный размер списка", file=sys.stderr)
         exit(1)
-
-    s = 0
-    for item in A:
-        if abs(item) < 5:
-            s += item
-
+    s = sum(a for a in A if abs(a) < 5)
     print(s)
-
-
-
-    
